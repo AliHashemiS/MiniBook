9cd4c7ada07e2dfdd4df82bc495134ab07198f87
Ali Hashemi Shirkavand <79598781+AliHashemiS@users.noreply.github.com>
2023-05-27 21:58:13 -0600

    Created Home page

---
+++ mini-book/components/button/Button.jsx
@@ -0,0 +1,9 @@
+import style from './Button.module.css'
+
+export function Button({buttonName, buttonlink}) {
+    return(
+        <a href={buttonlink} className={style.button}> 
+            {buttonName}
+        </a>
+    )
+}
---
+++ mini-book/components/shareIcons/ShareIcons.jsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from './ShareIcons.module.css'
+
+const dataFetch = () =>{
+    return fetch('http://localhost:3000/api/dataSocialMedia', { cache: 'no-store' })
+    .then(res => res.json())
+}
+
+export async function ShareIcons() {
+    const dataSocialMedia = await dataFetch()
+
+    return(
+        <div className={styles.socialWrapper}>
+            {dataSocialMedia.map(social => (
+                <Link href={social.link}>
+                    <FontAwesomeIcon className={styles.icon} icon={social.icon} />
+                </Link>
+            ))}
+        </div>
+    )
+}
