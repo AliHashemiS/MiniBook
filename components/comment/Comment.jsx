e97eac03976f29d3334f80c8f85d66870cc60240
Ali Hashemi Shirkavand <79598781+AliHashemiS@users.noreply.github.com>
2023-06-19 09:06:47 -0600

    Changed env variable.

--- components/comment/Comment.jsx
+++ components/comment/Comment.jsx
@@ -8,7 +8,7 @@ export default function Comment () {
           We want to hear from you. Share your thoughts about A Wearable World.
         </b>
       </p>
-      <form className={styles.form} action={process.env.COMMENT_URL} acceptCharset='UTF-8' method='post'>
+      <form className={styles.form} action='https://ali-hashemi.mykajabi.com/forms/2148178804/form_submissions' acceptCharset='UTF-8' method='post'>
         <div className={styles.formContainer}>
           <input name='utf8' type='hidden' value='&#x2713;' />
           <input id='form_submission_email' className={styles.inputEmail} type='email' size='30' required maxLength='256' name='form_submission[email]' placeholder='Enter your email...' />
--- components/subscription/Subscription.jsx
+++ components/subscription/Subscription.jsx
@@ -8,7 +8,7 @@ export default function Subscription () {
           Subscribe to get our latest content by email.
         </b>
       </p>
-      <form className={styles.form} action={process.env.SUBSCRIPTION_URL} acceptCharset='UTF-8' method='post'>
+      <form className={styles.form} action='https://ali-hashemi.mykajabi.com/forms/2148168308/form_submissions' acceptCharset='UTF-8' method='post'>
         <div className={styles.formContainer}>
           <input name='utf8' type='hidden' value='&#x2713;' />
           <input className={styles.inputEmail} type='email' size='30' required maxLength='256' name='form_submission[email]' placeholder='Enter your email...' />
