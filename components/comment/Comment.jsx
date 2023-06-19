45a4f674abdecded25eb332d1c9879a08c287284
Ali Hashemi Shirkavand <79598781+AliHashemiS@users.noreply.github.com>
2023-06-19 08:30:06 -0600

    Fixed lint errors.

--- components/comment/Comment.jsx
+++ components/comment/Comment.jsx
@@ -11,8 +11,8 @@ export default function Comment () {
       <form className={styles.form} action={process.env.COMMENT_URL} acceptCharset='UTF-8' method='post'>
         <div className={styles.formContainer}>
           <input name='utf8' type='hidden' value='&#x2713;' />
-          <input id="form_submission_email" className={styles.inputEmail} type='email' size='30' required maxLength='256' name='form_submission[email]' placeholder='Enter your email...' />
-          <textarea id="form_submission_custom_1" className={styles.inputComment} required name="form_submission[custom_1]" placeholder="Enter your comment..." />
+          <input id='form_submission_email' className={styles.inputEmail} type='email' size='30' required maxLength='256' name='form_submission[email]' placeholder='Enter your email...' />
+          <textarea id='form_submission_custom_1' className={styles.inputComment} required name='form_submission[custom_1]' placeholder='Enter your comment...' />
           <input className={styles.inputSubmit} type='submit' value='Send' />
         </div>
       </form>
